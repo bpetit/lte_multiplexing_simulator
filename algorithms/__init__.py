28a90f75b4bafc16dbc32f17c15f8b0b3d1baf69
bpetit <benoit.petit.info@gmail.com>
2013-07-06 14:47:07 +0200

    added graphs dir

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,2 +1,20 @@
+# This file is part of lte_multiplexing_simulator.
+#
+# lte_multiplexing_simulator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lte_multiplexing_simulator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2013 Benoit Petit, Guillaume Mazoyer
+#
+
 __all__ = ['round_robin', 'proportional_fair',
            'max_cqi']
--- settings.py
+++ settings.py
@@ -1,11 +1,29 @@
+# This file is part of lte_multiplexing_simulator.
+#
+# lte_multiplexing_simulator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lte_multiplexing_simulator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2013 Benoit Petit, Guillaume Mazoyer
+#
+
 # default settings
 from algorithms import round_robin, proportional_fair, max_cqi
 
 # the class of the algorithm that the scheduler as to use
 # don't suppress this one
-#SCHEDULER_ALGORITHM = round_robin.RoundRobin
+SCHEDULER_ALGORITHM = round_robin.RoundRobin
 #SCHEDULER_ALGORITHM = max_cqi.MaxCQI
-SCHEDULER_ALGORITHM = proportional_fair.ProportionalFair
+#SCHEDULER_ALGORITHM = proportional_fair.ProportionalFair
 
 RANDOM_CQI = True
 
